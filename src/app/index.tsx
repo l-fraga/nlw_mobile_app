335ca99d1e1c651cbc52be4c50cf9ade6ed320a9
Lucas <lucassilvacma@hotmail.com>
2024-02-09 14:12:00 -0300

    estruturação e estilização do cabeçalho

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,11 @@
-import { View, Text } from 'react-native'
+import { Header } from '@/components/header'
+import { View } from 'react-native'
 
 
 export default function Home() {
     return (
         <View className="flex-1 pt-8">
-            <Text className="text-white text-2xl">
-                React Native!
-            </Text>
+            <Header title= "Faça seu pedido" cartQuantity={1}/>
         </View>
-
     )
 }
