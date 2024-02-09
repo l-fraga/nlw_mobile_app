1834bbc077438c10e9d328b9f08074dfe386284c
Lucas <lucassilvacma@hotmail.com>
2024-02-09 11:46:37 -0300

    configurado ambiente de desenvolvimento

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+
+export default function Home() {
+    return (
+        <View className="flex-1 pt-8">
+            <Text className="text-white text-2xl">
+                React Native!
+            </Text>
+        </View>
+
+    )
+}
