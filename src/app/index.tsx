e001eb07a26bbfc73d28fd3902e5133dd915b1a0
Lucas <lucassilvacma@hotmail.com>
2024-02-09 15:18:00 -0300

    estilização botões de categorias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,24 @@
+import { View, FlatList } from 'react-native'
+
+import { CATEGORIES } from "@/utils/data/products"
 import { Header } from '@/components/header'
-import { View } from 'react-native'
+import { CategoryButton } from '@/components/category-button'
 
 
 export default function Home() {
     return (
         <View className="flex-1 pt-8">
-            <Header title= "Faça seu pedido" cartQuantity={1}/>
+            <Header title="Faça seu pedido" cartQuantity={1} />
+
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => <CategoryButton title={item} />}
+                horizontal
+                className="max-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
         </View>
     )
 }
