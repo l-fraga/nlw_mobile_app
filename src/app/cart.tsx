1b8bc59058081f5fd939f53b6cab3b5e5cb83b21
Lucas <lucassilvacma@hotmail.com>
2024-02-12 22:50:16 -0300

    ->configuração de limpar o carrinho ->API WHATSAPP

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,4 +1,6 @@
-import { View, Text, ScrollView, Alert } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, Alert, Linking } from "react-native";
+import { useNavigation } from "expo-router";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Feather } from "@expo/vector-icons";
 
@@ -10,9 +12,16 @@ import { Input } from "@/components/input";
 import { Button } from "@/components/button";
 import { LinkButton } from "@/components/link.button";
 
+/*
+const PHONE_NUMBER = "DDD PAIS DDD ESTADO NUMERO CELULAR"
+TUDO JUNTO SEM ESPAÇO NEM CARACTERES ESPECIAIS
+*/
+
 
 export default function Cart() {
+    const [address, setAddress] = useState("")
     const cartStore = useCartStore()
+    const navigation = useNavigation()
 
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
 
@@ -22,10 +31,32 @@ export default function Cart() {
                 text: "Cancelar",
             },
             {
-                text:"Remover",
+                text: "Remover",
                 onPress: () => cartStore.remove(product.id),
             }
         ])
+    }
+
+    function handleOrder() {
+        if (address.trim().length === 0) {
+            return Alert.alert("Pedido", "Informe os dados da entrega.")
+        }
+
+        const products = cartStore.products.map((product) => `\n${product.quantity} ${product.title}`).join("")
+
+        const message = `
+        🍔 NOVO PEDIDO
+        \nEntregar em: ${address}
+
+        ${products}
+
+        \nValor total: ${total}
+        `
+
+        Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+        cartStore.clear()
+        navigation.goBack()
     }
 
     return (
@@ -38,10 +69,10 @@ export default function Cart() {
                             <View className="border-b border-slate-700">
                                 {
                                     cartStore.products.map((product) => (
-                                        <Product 
-                                        key={product.id} 
-                                        data={product} 
-                                        onPress={() => handleProductRemove} />
+                                        <Product
+                                            key={product.id}
+                                            data={product}
+                                            onPress={() => handleProductRemove} />
                                     ))
                                 }
                             </View>
@@ -58,13 +89,18 @@ export default function Cart() {
                             <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
                         </View>
 
-                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+                            onChangeText={setAddress}
+                            blurOnSubmit={true}
+                            onSubmitEditing={handleOrder}
+                            returnKeyType="done"
+                        />
                     </View>
                 </ScrollView>
             </KeyboardAwareScrollView>
 
             <View className="p-5 gap-5">
-                <Button>
+                <Button onPress={handleOrder}>
                     <Button.Text>Enviar pedido</Button.Text>
                     <Button.Icon>
                         <Feather name="arrow-right-circle" size={20} />
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -37,9 +37,9 @@ export default function Product() {
                 resizeMode="cover"
             />
 
-            <Text className="text-white text-xl font-heading p-5">{product.title}</Text>
+            <Text className="text-white text-2xl font-heading p-5">{product.title}</Text>
 
-            <View className="p-5 mt-8 flex-1 ">
+            <View className="p-5 flex-1 ">
                 <Text className="text-lime-400 text-2xl font-heading my-2">
                     {formatCurrency(product.price)}
                 </Text>
